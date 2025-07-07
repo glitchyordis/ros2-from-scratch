06d1493ca44ebfdae402c112e18b35602dfa56dd
glitchyordis <99534415+glitchyordis@users.noreply.github.com>
2025-07-07 20:53:24 +0800

    ch 6 service servers

--- src/my_cpp_pkg/src/number_counter.cpp
+++ src/my_cpp_pkg/src/number_counter.cpp
@@ -1,7 +1,13 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "my_robot_interfaces/srv/reset_counter.hpp"
+
+// note you may need to include `"/home/<user>/ros2_ws/install/my_robot_interfaces/include/**` info c_cpp_properties.json
+// to make the code compile in VSCode, where `<user>` is your usernaame
 
 using namespace std::placeholders;
+using ResetCounter = my_robot_interfaces::srv::ResetCounter; //  using keyword so that we can just write ResetCounter in the code, instead of my_robot_interfaces::srv::ResetCounter:
+using Int64 = example_interfaces::msg::Int64;
 
 class NumberCounterNode : public rclcpp::Node
 {
@@ -12,7 +18,9 @@ public:
         // for _1 to work we need `using namespace std::placeholders;` before it
         this->create_subscription<example_interfaces::msg::Int64>(
                 "number", 10, std::bind(&NumberCounterNode::callbackNumber, this, _1));
-            
+        
+        reset_counter_service_ = this->create_service<ResetCounter>(
+            "reset_counter", std::bind(&NumberCounterNode::callbackResetCounter, this, _1, _2));
         RCLCPP_INFO(this->get_logger(), "Number Counter has been started.");
     }
 
@@ -25,8 +33,28 @@ private:
         RCLCPP_INFO(this->get_logger(), "Counter: %d", counter_);
     }
 
+    void callbackResetCounter(const ResetCounter::Request::SharedPtr request, const ResetCounter::Response::SharedPtr response)
+    {
+        if (request->reset_value < 0) {
+            response->success = false;
+            response->message = "Cannot reset counter to a negative value";
+        }
+        else if (request->reset_value > counter_) {
+            response->success = false;
+            response->message = "Reset value must be lower than current counter value";
+        }
+        else {
+            counter_ = request->reset_value;
+            RCLCPP_INFO(this->get_logger(), "Reset counter to %d", counter_);
+            response->success = true;
+            response->message = "Success";
+        }
+    }
+
+
     int counter_;
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr number_subscriber_;
+    rclcpp::Service<ResetCounter>::SharedPtr reset_counter_service_;
 };
 
 int main(int argc, char **argv)
--- src/my_py_pkg/my_py_pkg/number_counter.py
+++ src/my_py_pkg/my_py_pkg/number_counter.py
@@ -3,6 +3,8 @@ import rclpy
 from example_interfaces.msg import Int64  # same as publisher
 from rclpy.node import Node
 
+from my_robot_interfaces.srv import ResetCounter
+
 
 class NumberCounterNode(Node):
     def __init__(self):
@@ -13,6 +15,13 @@ class NumberCounterNode(Node):
         self.number_subscriber = self.number_subscriber_ = (
             self.create_subscription(Int64, "number", self.callback_number, 10)
         )
+        
+        self.reset_counter_service_ = self.create_service(
+            ResetCounter, # the service interface
+            "reset_counter",  # the service name
+            self.callback_reset_counter
+        )
+            
         self.get_logger().info("Number Counter has been started.")
 
     def callback_number(self, msg: Int64):
@@ -20,6 +29,19 @@ class NumberCounterNode(Node):
         self.counter_ += msg.data
         self.get_logger().info("Counter:  " + str(self.counter_))
 
+    def callback_reset_counter(self, request: ResetCounter.Request, response: ResetCounter.Response):
+        if request.reset_value < 0:
+            response.success = False
+            response.message = "Cannot reset counter to a negative value"
+        elif request.reset_value > self.counter_:
+            response.success = False
+            response.message = "Reset value must be lower than current counter value"
+        else:
+            self.counter_ = request.reset_value
+            self.get_logger().info("Reset counter to " + str(self.counter_))
+            response.success = True
+            response.message = "Success"
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
